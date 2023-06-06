b8a78c368d65e4c0e496826ff2663523167ee990
Dev-Aligator <devaligator1004@gmail.com>
2023-06-06 21:40:29 +0700

    Allow login with github, facebook

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.microsoft',
+    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.github',
 ]
 
